1ae9122001ed138cd52f7500492b17b1d238cede
suga <>
2023-06-29 11:43:00 +0900

    Chapter1-6を作成

---
+++ packeges/javascript-curriculum-expert/src/pages/Chapter1/Chapter1_6/index.tsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react"
+import CommonHeader from "../../../components/CommonHeader"
+import { Chapter1_6 } from "../../../components/Chapters/Chapter1/Chapter1_6"
+
+const Chapter1_5Pages = () => {
+  return (
+    <Fragment>
+      <CommonHeader />
+      <Chapter1_6 />
+    </Fragment>
+  )
+}
+
+export default Chapter1_5Pages
